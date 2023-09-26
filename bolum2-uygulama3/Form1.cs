e884a2e47288d15d62c623b697aa408fc769eba1
nurrllyy <114949657+nurrllyy@users.noreply.github.com>
2023-09-26 10:14:56 +0300

    Add files via upload

---
+++ bolum2-uygulama3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum2_uygulama3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte sayi1, sayi2;
+            sayi1 = Convert.ToByte(textBox1.Text);
+            sayi2 = Convert.ToByte(textBox2.Text);
+
+            if (sayi1 > sayi2)
+            {
+                MessageBox.Show("1.Sayı 2.Sayıdan büyüktür");
+            }
+            if(sayi1 < sayi2)
+            {
+                MessageBox.Show("1.Sayı 2.Sayıdan Küçüktür");
+            }
+            if (sayi1 == sayi2)
+            {
+                MessageBox.Show("Sayılar birbirine eşit");
+            }
+
+        }
+    }
+}
